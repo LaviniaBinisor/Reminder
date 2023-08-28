ecd88fbfed73e26beed4d1b44c5fae8c60e7f41f
Binisor Lavinia <maria.lavinia.nita@gmail.com>
2023-08-28 12:04:00 +0300

    Initial Commit

---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by Binisor, Ionut Aurel (Cognizant) on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
